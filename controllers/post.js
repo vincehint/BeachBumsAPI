d96cb7c47b28c23b4835d6875539dcdbd4554df4
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-02-22 13:10:03 -0600

    working on post

---
+++ controllers/post.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const { createUserToken } = require('../middleware/auth')
+const passport = require("passport")
+
+
+
+
+
+module.exports = router
